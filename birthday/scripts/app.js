5f635fb08ea48e2110a30a80cafb1d4d5bbaaebd
Sonia Boller <soniaboller@gmail.com>
2017-03-01 17:31:02 -0500

    this is hilarious

--- birthday/scripts/app.js
+++ birthday/scripts/app.js
@@ -20,6 +20,23 @@ function init() {
     // Lights
     var ambient = new THREE.AmbientLight( 0xffffff );
     scene.add( ambient );
+    // var dirLight = new THREE.DirectionalLight( 0xff0000, 0.5 );
+    // dirLight.position.set( 0, 1, 0 ).normalize();
+    // scene.add( dirLight );
+
+    var pointLight = new THREE.PointLight( 0x0000ff, 10, 400 );
+    pointLight.position.set( 0, 300, 100 );
+    scene.add( pointLight );
+
+    var pointLight2 = new THREE.PointLight( 0xff0000, 10, 400 );
+    pointLight2.position.set( 0, 300, -100 );
+    scene.add( pointLight2 );
+
+
+    var pointLight3 = new THREE.PointLight( 0x00ff00, 10, 400 );
+    pointLight3.position.set( 0, -10, 0 );
+    scene.add( pointLight3 );
+
     // Textures
     var textureLoader = new THREE.TextureLoader();
     textureEquirec = textureLoader.load( "textures/sikkim.jpg" );
@@ -31,27 +48,67 @@ function init() {
 
     ianTexture = textureLoader.load( "textures/ian1.png" );
     soniaTexture = textureLoader.load( "textures/sonia1.png" );
-    
-    var ianMaterial = new THREE.PointsMaterial({ size: 150, map: ianTexture, blending: THREE.AdditiveBlending, depthTest: false, transparent : false });
-    var soniaMaterial = new THREE.PointsMaterial({ size: 150, map: soniaTexture, blending: THREE.AdditiveBlending, depthTest: false, transparent : true});
-    var ianGeometry = new THREE.Geometry();
-    var ianVertex = new THREE.Vector3();
-    ianVertex.x = -500;
-    ianVertex.y = 100;
-    ianVertex.z = 1;
-    ianGeometry.vertices.push(ianVertex);
 
-    var soniaGeometry = new THREE.Geometry();
-    var soniaVertex = new THREE.Vector3();
-    soniaVertex.x = 500;
-    soniaVertex.y = 100;
-    soniaVertex.z = 1;
-    soniaGeometry.vertices.push(soniaVertex);
+    // var ianMaterial = new THREE.SpriteMaterial({ size: 150, map: ianTexture, blending: THREE.AdditiveBlending, depthTest: false, transparent : true });
+    // var soniaMaterial = new THREE.SpriteMaterial({ size: 150, map: soniaTexture, blending: THREE.AdditiveBlending, depthTest: false, transparent : true});
 
-    ian = new THREE.Points (ianGeometry, ianMaterial);
-    sonia = new THREE.Points (soniaGeometry, soniaMaterial);
+    var ianMaterial = new THREE.SpriteMaterial({map:ianTexture, color:0xffffff});
+    var soniaMaterial = new THREE.SpriteMaterial({map:soniaTexture, color:0xffffff});
+
+    // var ianGeometry = new THREE.Geometry();
+    // var ianVertex = new THREE.Vector3();
+    // ianVertex.x = -500;
+    // ianVertex.y = 100;
+    // ianVertex.z = 1;
+    // ianGeometry.vertices.push(ianVertex);
+    //
+    // var soniaGeometry = new THREE.Geometry();
+    // var soniaVertex = new THREE.Vector3();
+    // soniaVertex.x = 500;
+    // soniaVertex.y = 100;
+    // soniaVertex.z = 1;
+    // soniaGeometry.vertices.push(soniaVertex);
+
+    // ian = new THREE.Points (ianGeometry, ianMaterial);
+    // sonia = new THREE.Points (soniaGeometry, soniaMaterial);
+
+    ian = new THREE.Sprite( ianMaterial );
+    ian.scale.set(150, 200, 0)
+    ian.position.set(500,200,1)
+    sonia = new THREE.Sprite( soniaMaterial );
+    sonia.scale.set(150, 200, 0)
+    sonia.position.set(-500,200,1)
 
     // Materials
+
+
+    var loader = new THREE.FontLoader();
+
+    loader.load( 'fonts/helvetiker_regular.typeface.json', function ( font ) {
+
+        var textGeo = new THREE.TextGeometry( "HAPPY BIRTHDAY", {
+
+            font: font,
+
+            size: 50,
+            height: 50,
+            curveSegments: 12,
+
+            bevelThickness: 1,
+            bevelSize: 1,
+            bevelEnabled: true
+
+        } );
+
+        var textMaterial = new THREE.MeshPhongMaterial( { color: 0x666666 } );
+
+        var birthday = new THREE.Mesh( textGeo, textMaterial );
+        birthday.position.set( -300, 100, 10 );
+
+        scene.add( birthday );
+
+    } );
+
 
     var equirectShader = THREE.ShaderLib[ "equirect" ];
     var equirectMaterial = new THREE.ShaderMaterial( {
