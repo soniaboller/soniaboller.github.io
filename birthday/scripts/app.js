988e46da17a8c7dd776deadd1591dfab5fefd211
Sonia Boller <soniaboller@gmail.com>
2017-03-01 14:39:22 -0500

    update

--- birthday/scripts/app.js
+++ birthday/scripts/app.js
@@ -1,6 +1,6 @@
 var controls, camera, scene, renderer;
 var cameraCube, sceneCube;
-var textureEquirec, textureCube, textureSphere;
+var textureEquirec, textureSphere;
 var cubeMesh, sphereMesh;
 var sphereMaterial;
 init();
@@ -20,13 +20,6 @@ function init() {
     var ambient = new THREE.AmbientLight( 0xffffff );
     scene.add( ambient );
     // Textures
-    var r = "textures/";
-    var urls = [ r + "sikkim.jpg", r + "sikkim.jpg",
-        r + "sikkim.jpg", r + "sikkim.jpg",
-        r + "sikkim.jpg", r + "sikkim.jpg" ];
-    textureCube = new THREE.CubeTextureLoader().load( urls );
-    textureCube.format = THREE.RGBFormat;
-    textureCube.mapping = THREE.CubeReflectionMapping;
     var textureLoader = new THREE.TextureLoader();
     textureEquirec = textureLoader.load( "textures/sikkim.jpg" );
     textureEquirec.mapping = THREE.EquirectangularReflectionMapping;
@@ -53,15 +46,18 @@ function init() {
         depthWrite: false,
         side: THREE.BackSide
     } );
-    cubeMaterial.uniforms[ "tCube" ].value = textureCube;
+    cubeMaterial.uniforms[ "tCube" ].value = textureEquirec;
     // Skybox
-    cubeMesh = new THREE.Mesh( new THREE.BoxGeometry( 100, 100, 100 ), cubeMaterial );
+    cubeMesh = new THREE.Mesh( new THREE.BoxGeometry( 100, 100, 100 ), equirectMaterial );
     sceneCube.add( cubeMesh );
     //
     var geometry = new THREE.SphereGeometry( 400.0, 24, 24 );
-    sphereMaterial = new THREE.MeshLambertMaterial( { envMap: textureCube } );
+    sphereMaterial = new THREE.MeshLambertMaterial( { envMap: textureEquirec } );
     sphereMesh = new THREE.Mesh( geometry, sphereMaterial );
     scene.add( sphereMesh );
+
+    cubeMesh.visible = true;
+    sphereMaterial.needsUpdate = true;
     //
     renderer = new THREE.WebGLRenderer();
     renderer.autoClear = false;
@@ -70,42 +66,6 @@ function init() {
     renderer.setFaceCulling( THREE.CullFaceNone );
     document.body.appendChild( renderer.domElement );
     //
-    var params = {
-        Cube: function () {
-            cubeMesh.material = cubeMaterial;
-            cubeMesh.visible = true;
-            sphereMaterial.envMap = textureCube;
-            sphereMaterial.needsUpdate = true;
-        },
-        Equirectangular: function () {
-            cubeMesh.material = equirectMaterial;
-            cubeMesh.visible = true;
-            sphereMaterial.envMap = textureEquirec;
-            sphereMaterial.needsUpdate = true;
-        },
-        Spherical: function () {
-            cubeMesh.visible = false;
-            sphereMaterial.envMap = textureSphere;
-            sphereMaterial.needsUpdate = true;
-        },
-        Refraction: false
-    };
-    var gui = new dat.GUI();
-    gui.add( params, 'Cube' );
-    gui.add( params, 'Equirectangular' );
-    gui.add( params, 'Spherical' );
-    gui.add( params, 'Refraction' ).onChange( function( value ) {
-        if ( value ) {
-            textureEquirec.mapping = THREE.EquirectangularRefractionMapping;
-            textureCube.mapping = THREE.CubeRefractionMapping;
-        } else {
-            textureEquirec.mapping = THREE.EquirectangularReflectionMapping;
-            textureCube.mapping = THREE.CubeReflectionMapping;
-        }
-        sphereMaterial.needsUpdate = true;
-    } );
-    gui.open();
-    window.addEventListener( 'resize', onWindowResize, false );
 }
 function onWindowResize() {
     camera.aspect = window.innerWidth / window.innerHeight;
